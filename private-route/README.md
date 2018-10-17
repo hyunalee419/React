7b31a4d8fd1994239e5f0caf672f29fa0c940803
hyunalee <hyunalee419@cashwalk.io>
2018-10-17 16:02:37 +0900

    feat(private-route): add private-route

---
+++ private-route/README.md
@@ -0,0 +1,6 @@
+이 프로젝트는 [Create React App](https://github.com/facebook/create-react-app)로 프로젝트 생성 및 [auth-workflow](
+)를 기반으로 만든 예제 프로젝트입니다.
+
+로그인 후 페이지 권한 관련 기능은 해당 프로젝트 방식으로 해결하면 됩니다.
+
+자세한 설명은 [Protected routes and authentication with React Router v4](https://tylermcginnis.com/react-router-protected-routes-authentication/)를 참고하세요.
---
+++ private-route/src/App.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from 'react-router-dom';
+
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true
+    setTimeout(cb, 100) // fake async
+  },
+  signout(cb) {
+    this.isAuthenticated = false
+    setTimeout(cb, 100) // fake async
+  }
+}
+
+const Public = () => <div>Public page</div>;
+const Protected = () => <div>Protected page</div>;
+
+class Login extends Component {
+  state = {
+    redirectToReferrer: false
+  }
+  login = () => {
+    fakeAuth.authenticate(() => {
+      this.setState({redirectToReferrer: true})
+    })
+  }
+  render() {
+    const { from } = this.props.location.state || { from: { pathname: '/' } }
+    const { redirectToReferrer } = this.state
+
+    if (redirectToReferrer === true) {
+      return <Redirect to={from} />
+    }
+    return (
+      <div>
+        <p>You must log in to view the page</p>
+        <button onClick={this.login}>Log in</button>
+      </div>
+    )
+  }
+}
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={(props) => (
+    fakeAuth.isAuthenticated === true
+      ? <Component {...props} />
+      : <Redirect to="/login"/>
+  )} />
+)
+
+const AuthButton = withRouter(({ history }) => (
+  fakeAuth.isAuthenticated ? (
+    <p>
+      Welcome! <button onClick={() => {
+        fakeAuth.signout(() => history.push('/'))
+    }}>Sign out</button>
+    </p>
+  ) : (
+    <p>You are not logged in.</p>
+  )
+))
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <AuthButton/>
+          <ul>
+            <li>
+              <Link to="/public">Public Page</Link>
+            </li>
+            <li>
+              <Link to="/protected">Protected Page</Link>
+            </li>
+          </ul>
+          <Route path="/public" component={Public} />
+          <Route path="/login" component={Login} />
+          <PrivateRoute path='/protected' component={Protected} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
