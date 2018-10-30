17f759d567bb5bbafba1553e4da982db3f420576
hyunalee <hyunalee419@cashwalk.io>
2018-10-30 16:22:06 +0900

    feat(react-redux): add counter list

---
+++ react-redux/README.md
@@ -0,0 +1,3 @@
+[리액트를 다루는 기술](https://github.com/velopert/learning-react) 책 13장에 있는 예제 코드입니다.
+
+react-redux 를 사용한 counter list 샘플 코드입니다.
---
+++ react-redux/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+import './index.css';
+
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Provider } from 'react-redux';
+
+const store = createStore(reducers, window.devToolsExtension && window.devToolsExtension());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
