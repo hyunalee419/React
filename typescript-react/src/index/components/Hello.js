5213457fa22016d35004e1914bece94d79e2d757
hyunalee <hyunalee419@cashwalk.io>
2019-01-09 17:10:49 +0900

    feat(typescript): init typescript-react project

---
+++ typescript-react/src/index/components/Hello.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Hello extends Component {
+
+	componentDidMount() {
+		console.log('good');
+	}
+
+	render() {
+		return (
+			<div>
+				캐시워크
+			</div>
+		)
+	}
+}
